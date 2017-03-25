2e6fe09af0eb15f439b0ec3d9275437649830392
aleksapestalic <aleksapestalic@gmail.com>
2017-03-25 22:43:43 +0100

    Add files via upload
    A trigonometry application, made out of boredom. It demonstrates how basic trigonometry works, and shows the calculated values for the current cursor position. A fun application, but a bit complicated for developing.

---
+++ Trigonometry/Trigonometry/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Trigonometry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lLocationX = new System.Windows.Forms.Label();
+            this.lLocationY = new System.Windows.Forms.Label();
+            this.lB = new System.Windows.Forms.Label();
+            this.lC = new System.Windows.Forms.Label();
+            this.lAlpha = new System.Windows.Forms.Label();
+            this.lA = new System.Windows.Forms.Label();
+            this.lGama = new System.Windows.Forms.Label();
+            this.lSinAlpha = new System.Windows.Forms.Label();
+            this.lBeta = new System.Windows.Forms.Label();
+            this.lSinBeta = new System.Windows.Forms.Label();
+            this.lSinGama = new System.Windows.Forms.Label();
+            this.lCosGama = new System.Windows.Forms.Label();
+            this.lCosBeta = new System.Windows.Forms.Label();
+            this.lCosAlpha = new System.Windows.Forms.Label();
+            this.lTanGama = new System.Windows.Forms.Label();
+            this.lTanBeta = new System.Windows.Forms.Label();
+            this.lTanAlpha = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbCircle = new System.Windows.Forms.CheckBox();
+            this.lWarning = new System.Windows.Forms.Label();
+            this.pbWarning = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWarning)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lLocationX
+            // 
+            this.lLocationX.AutoSize = true;
+            this.lLocationX.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lLocationX.Location = new System.Drawing.Point(6, 19);
+            this.lLocationX.Name = "lLocationX";
+            this.lLocationX.Size = new System.Drawing.Size(16, 16);
+            this.lLocationX.TabIndex = 0;
+            this.lLocationX.Text = "X";
+            // 
+            // lLocationY
+            // 
+            this.lLocationY.AutoSize = true;
+            this.lLocationY.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lLocationY.Location = new System.Drawing.Point(6, 35);
+            this.lLocationY.Name = "lLocationY";
+            this.lLocationY.Size = new System.Drawing.Size(17, 16);
+            this.lLocationY.TabIndex = 1;
+            this.lLocationY.Text = "Y";
+            // 
+            // lB
+            // 
+            this.lB.AutoSize = true;
+            this.lB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lB.Location = new System.Drawing.Point(3, 75);
+            this.lB.Name = "lB";
+            this.lB.Size = new System.Drawing.Size(16, 16);
+            this.lB.TabIndex = 2;
+            this.lB.Text = "b";
+            // 
+            // lC
+            // 
+            this.lC.AutoSize = true;
+            this.lC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lC.Location = new System.Drawing.Point(4, 91);
+            this.lC.Name = "lC";
+            this.lC.Size = new System.Drawing.Size(15, 16);
+            this.lC.TabIndex = 3;
+            this.lC.Text = "c";
+            // 
+            // lAlpha
+            // 
+            this.lAlpha.AutoSize = true;
+            this.lAlpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lAlpha.Location = new System.Drawing.Point(4, 118);
+            this.lAlpha.Name = "lAlpha";
+            this.lAlpha.Size = new System.Drawing.Size(43, 16);
+            this.lAlpha.TabIndex = 4;
+            this.lAlpha.Text = "Alpha";
+            // 
+            // lA
+            // 
+            this.lA.AutoSize = true;
+            this.lA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lA.Location = new System.Drawing.Point(3, 59);
+            this.lA.Name = "lA";
+            this.lA.Size = new System.Drawing.Size(16, 16);
+            this.lA.TabIndex = 5;
+            this.lA.Text = "a";
+            // 
+            // lGama
+            // 
+            this.lGama.AutoSize = true;
+            this.lGama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lGama.Location = new System.Drawing.Point(2, 150);
+            this.lGama.Name = "lGama";
+            this.lGama.Size = new System.Drawing.Size(56, 16);
+            this.lGama.TabIndex = 6;
+            this.lGama.Text = "Gamma";
+            // 
+            // lSinAlpha
+            // 
+            this.lSinAlpha.AutoSize = true;
+            this.lSinAlpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lSinAlpha.Location = new System.Drawing.Point(105, 19);
+            this.lSinAlpha.Name = "lSinAlpha";
+            this.lSinAlpha.Size = new System.Drawing.Size(65, 16);
+            this.lSinAlpha.TabIndex = 7;
+            this.lSinAlpha.Text = "Sin Alpha";
+            // 
+            // lBeta
+            // 
+            this.lBeta.AutoSize = true;
+            this.lBeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBeta.Location = new System.Drawing.Point(4, 134);
+            this.lBeta.Name = "lBeta";
+            this.lBeta.Size = new System.Drawing.Size(36, 16);
+            this.lBeta.TabIndex = 8;
+            this.lBeta.Text = "Beta";
+            // 
+            // lSinBeta
+            // 
+            this.lSinBeta.AutoSize = true;
+            this.lSinBeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lSinBeta.Location = new System.Drawing.Point(105, 34);
+            this.lSinBeta.Name = "lSinBeta";
+            this.lSinBeta.Size = new System.Drawing.Size(58, 16);
+            this.lSinBeta.TabIndex = 9;
+            this.lSinBeta.Text = "Sin Beta";
+            // 
+            // lSinGama
+            // 
+            this.lSinGama.AutoSize = true;
+            this.lSinGama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lSinGama.Location = new System.Drawing.Point(105, 50);
+            this.lSinGama.Name = "lSinGama";
+            this.lSinGama.Size = new System.Drawing.Size(67, 16);
+            this.lSinGama.TabIndex = 10;
+            this.lSinGama.Text = "Sin Gama";
+            // 
+            // lCosGama
+            // 
+            this.lCosGama.AutoSize = true;
+            this.lCosGama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lCosGama.Location = new System.Drawing.Point(105, 107);
+            this.lCosGama.Name = "lCosGama";
+            this.lCosGama.Size = new System.Drawing.Size(72, 16);
+            this.lCosGama.TabIndex = 14;
+            this.lCosGama.Text = "Cos Gama";
+            // 
+            // lCosBeta
+            // 
+            this.lCosBeta.AutoSize = true;
+            this.lCosBeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lCosBeta.Location = new System.Drawing.Point(105, 91);
+            this.lCosBeta.Name = "lCosBeta";
+            this.lCosBeta.Size = new System.Drawing.Size(63, 16);
+            this.lCosBeta.TabIndex = 13;
+            this.lCosBeta.Text = "Cos Beta";
+            // 
+            // lCosAlpha
+            // 
+            this.lCosAlpha.AutoSize = true;
+            this.lCosAlpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lCosAlpha.Location = new System.Drawing.Point(105, 75);
+            this.lCosAlpha.Name = "lCosAlpha";
+            this.lCosAlpha.Size = new System.Drawing.Size(70, 16);
+            this.lCosAlpha.TabIndex = 12;
+            this.lCosAlpha.Text = "Cos Alpha";
+            // 
+            // lTanGama
+            // 
+            this.lTanGama.AutoSize = true;
+            this.lTanGama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTanGama.Location = new System.Drawing.Point(105, 166);
+            this.lTanGama.Name = "lTanGama";
+            this.lTanGama.Size = new System.Drawing.Size(72, 16);
+            this.lTanGama.TabIndex = 17;
+            this.lTanGama.Text = "Tan Gama";
+            // 
+            // lTanBeta
+            // 
+            this.lTanBeta.AutoSize = true;
+            this.lTanBeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTanBeta.Location = new System.Drawing.Point(105, 150);
+            this.lTanBeta.Name = "lTanBeta";
+            this.lTanBeta.Size = new System.Drawing.Size(63, 16);
+            this.lTanBeta.TabIndex = 16;
+            this.lTanBeta.Text = "Tan Beta";
+            // 
+            // lTanAlpha
+            // 
+            this.lTanAlpha.AutoSize = true;
+            this.lTanAlpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTanAlpha.Location = new System.Drawing.Point(105, 134);
+            this.lTanAlpha.Name = "lTanAlpha";
+            this.lTanAlpha.Size = new System.Drawing.Size(70, 16);
+            this.lTanAlpha.TabIndex = 15;
+            this.lTanAlpha.Text = "Tan Alpha";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbCircle);
+            this.groupBox1.Controls.Add(this.lA);
+            this.groupBox1.Controls.Add(this.lTanGama);
+            this.groupBox1.Controls.Add(this.lLocationX);
+            this.groupBox1.Controls.Add(this.lTanBeta);
+            this.groupBox1.Controls.Add(this.lTanAlpha);
+            this.groupBox1.Controls.Add(this.lLocationY);
+            this.groupBox1.Controls.Add(this.lB);
+            this.groupBox1.Controls.Add(this.lCosGama);
+            this.groupBox1.Controls.Add(this.lC);
+            this.groupBox1.Controls.Add(this.lCosBeta);
+            this.groupBox1.Controls.Add(this.lCosAlpha);
+            this.groupBox1.Controls.Add(this.lAlpha);
+            this.groupBox1.Controls.Add(this.lBeta);
+            this.groupBox1.Controls.Add(this.lSinGama);
+            this.groupBox1.Controls.Add(this.lGama);
+            this.groupBox1.Controls.Add(this.lSinBeta);
+            this.groupBox1.Controls.Add(this.lSinAlpha);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 222);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DATA";
+            // 
+            // cbCircle
+            // 
+            this.cbCircle.AutoSize = true;
+            this.cbCircle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCircle.Location = new System.Drawing.Point(9, 190);
+            this.cbCircle.Name = "cbCircle";
+            this.cbCircle.Size = new System.Drawing.Size(61, 20);
+            this.cbCircle.TabIndex = 18;
+            this.cbCircle.Text = "Circle";
+            this.cbCircle.UseVisualStyleBackColor = true;
+            // 
+            // lWarning
+            // 
+            this.lWarning.AutoSize = true;
+            this.lWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lWarning.Location = new System.Drawing.Point(61, 445);
+            this.lWarning.Name = "lWarning";
+            this.lWarning.Size = new System.Drawing.Size(557, 18);
+            this.lWarning.TabIndex = 19;
+            this.lWarning.Text = "Warning, the cursor is outside the recommended area, errors may occur!";
+            // 
+            // pbWarning
+            // 
+            this.pbWarning.Image = ((System.Drawing.Image)(resources.GetObject("pbWarning.Image")));
+            this.pbWarning.Location = new System.Drawing.Point(12, 435);
+            this.pbWarning.Name = "pbWarning";
+            this.pbWarning.Size = new System.Drawing.Size(47, 37);
+            this.pbWarning.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbWarning.TabIndex = 20;
+            this.pbWarning.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 481);
+            this.Controls.Add(this.pbWarning);
+            this.Controls.Add(this.lWarning);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Trigonometry basics";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWarning)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lLocationX;
+        private System.Windows.Forms.Label lLocationY;
+        private System.Windows.Forms.Label lB;
+        private System.Windows.Forms.Label lC;
+        private System.Windows.Forms.Label lAlpha;
+        private System.Windows.Forms.Label lA;
+        private System.Windows.Forms.Label lGama;
+        private System.Windows.Forms.Label lSinAlpha;
+        private System.Windows.Forms.Label lBeta;
+        private System.Windows.Forms.Label lSinBeta;
+        private System.Windows.Forms.Label lSinGama;
+        private System.Windows.Forms.Label lCosGama;
+        private System.Windows.Forms.Label lCosBeta;
+        private System.Windows.Forms.Label lCosAlpha;
+        private System.Windows.Forms.Label lTanGama;
+        private System.Windows.Forms.Label lTanBeta;
+        private System.Windows.Forms.Label lTanAlpha;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lWarning;
+        private System.Windows.Forms.PictureBox pbWarning;
+        private System.Windows.Forms.CheckBox cbCircle;
+        private System.Windows.Forms.Timer timer1;
+
+
+    }
+}
+
---
+++ Trigonometry/Trigonometry/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trigonometry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void CrtanjeKruz(int x, int y, Graphics g)
+        {
+            Pen pen = new Pen(Color.Red, 2);
+            Pen penThin = new Pen(Color.Red, 1);
+            g.DrawEllipse(pen, x, y, ClientRectangle.Width / 2, ClientRectangle.Width / 2);
+            Point UpperV = new Point(x + ClientRectangle.Width / 4, y);
+            Point LowerV = new Point(x + ClientRectangle.Width / 4, y + ClientRectangle.Width / 2);
+            g.DrawLine(penThin, UpperV, LowerV);
+            Point LeftH = new Point(x, y + ClientRectangle.Width / 4);
+            Point RightH = new Point(x + ClientRectangle.Width / 2, y + ClientRectangle.Width / 4);
+            g.DrawLine(penThin, LeftH, RightH);
+        }
+
+        void BezKruga(int x, int y, Graphics g)
+        {
+            Pen pen = new Pen(Color.Red, 2);
+            Pen penThin = new Pen(Color.Red, 1);
+            //bez kruga
+            Point center = new Point(ClientRectangle.Width / 2 + 90, ClientRectangle.Height / 2);
+            Point yLOW = new Point(center.X, center.Y + ClientRectangle.Width / 4);
+            Point yUPPER = new Point(center.X, center.Y - ClientRectangle.Width / 4);
+            Point xR = new Point(center.X + ClientRectangle.Width / 4, center.Y);
+            Point xL = new Point(center.X - ClientRectangle.Width / 4, center.Y);
+
+            g.DrawLine(pen, yLOW, yUPPER);
+            g.DrawLine(pen, xL, xR);
+
+            Point LeftH = new Point(x, y + ClientRectangle.Width / 4);
+            Point RightH = new Point(x + ClientRectangle.Width / 2, y + ClientRectangle.Width / 4);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbWarning.Hide();
+            lWarning.Hide();
+            lWarning.Location = new Point(55, ClientRectangle.Height - 30);
+            pbWarning.Location = new Point(7, ClientRectangle.Height - 45);
+          
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            int x = ClientRectangle.Width / 2 - (ClientRectangle.Width / 2) / 2 + 90;
+            int y = ClientRectangle.Height / 2 - (ClientRectangle.Width / 2) / 2;
+            BezKruga(x, y, g);
+            if (cbCircle.Checked)
+            {
+                timer1.Start();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Refresh();
+            lWarning.Location = new Point(55, ClientRectangle.Height - 30);
+            pbWarning.Location = new Point(7, ClientRectangle.Height - 45);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int xK = ClientRectangle.Width / 2 - (ClientRectangle.Width / 2) / 2; //x Kruznica
+            int yK = ClientRectangle.Height / 2 - (ClientRectangle.Width / 2) / 2; //y Kruznica
+            Refresh();
+            int x = e.X; int y = e.Y;
+            Point XY = new Point(x, y);
+            /*
+             * int x = ClientRectangle.Width / 2 - (ClientRectangle.Width / 2) / 2;
+            int y = ClientRectangle.Height / 2 - (ClientRectangle.Width / 2) / 2;
+             * */
+            int xC = (ClientRectangle.Width / 2 - ClientRectangle.Width / 4) + ClientRectangle.Width / 4 + 90;
+            int yC = (ClientRectangle.Height / 2 - ClientRectangle.Width / 4) + ClientRectangle.Width / 4;
+            Graphics g = CreateGraphics();
+            Pen pen = new Pen(Color.Black, 4);
+            Point center = new Point(xC, yC);
+            g.DrawLine(pen, xC, yC, x, y);
+
+            //cursor
+            int xNC = x; //x Non Constant
+            int yCONST = yC; //y Constant
+
+            Point ptNC = new Point(xNC, yCONST);
+            g.DrawLine(pen, center, ptNC);
+
+            //spajanje i formiranje trougla
+            g.DrawLine(pen, XY, ptNC);
+
+            //uslovi za rad (abs x>r)
+            double a = ptNC.Y - y;
+            double b = ptNC.X - xC;
+            double c = Math.Sqrt(a * a + b * b);
+            
+            if (c > ClientRectangle.Width / 4)
+            {
+                pbWarning.Show();
+                lWarning.Show();
+            }
+            else
+            {
+                lWarning.Hide();
+                pbWarning.Hide();
+            }
+
+            //DATA CALCULATION
+            //location
+            lLocationX.Text = "X = " + x + " px";
+            lLocationY.Text = "Y = " + y + " px";
+            //stranice u px
+            double strA = ptNC.Y - y;
+            double strB = ptNC.X - xC;
+            double strC = Math.Sqrt(a * a + b * b);
+            //str u cm (1px = 0.026458cm)
+            const double cm = 0.026458;
+            double aCM = Math.Abs(strA * cm);
+            double bCm = Math.Abs(strB * cm);
+            double cCM = Math.Abs(strC * cm);
+            //prikaz u labelama
+            lA.Text = "a = " + aCM.ToString("0.00") + " cm";
+            lB.Text = "b = " + bCm.ToString("0.00") + " cm";
+            lC.Text = "x = " + cCM.ToString("0.00") + " cm";
+            //calc COS
+            double cosA = (bCm * bCm + cCM * cCM - aCM * aCM) / (2 * bCm * cCM); //po pravilu kosinusa
+            double cosB = (cCM * cCM + aCM * aCM - bCm * bCm) / (2 * aCM * cCM);
+            double cosC = (aCM * aCM + bCm * bCm - cCM * cCM) / (2 * aCM * bCm);
+            lCosAlpha.Text = "Cos 	α = " + cosA.ToString("0.00");
+            lCosBeta.Text = "Cos 	β = " + cosB.ToString("0.00");
+            lCosGama.Text = "Cos 	γ = " + cosC.ToString("0.00"); //uvek je 0 (cos 90 = 0)
+            //angles in deg
+            const double deg = 57.2958;
+            double angA = Math.Acos(cosA) * deg; //from rad to deg
+            double angB = Math.Acos(cosB) * deg;
+            double angC = Math.Acos(cosC) * deg;
+            lAlpha.Text = "α = " + (angA).ToString("0.00") + " deg";
+            lBeta.Text = "β = " + (angB).ToString("0.00") + " deg";
+            lGama.Text = "γ = " + (angC).ToString("0.00") + " deg";
+
+            //sin alpha, beta, gamma;
+            double sinA = Math.Abs(Math.Sin(angA));
+            double sinB = Math.Abs(Math.Sin(angB));
+            double sinC = Math.Abs(Math.Sin(angC));
+            lSinAlpha.Text = "Sin α = " + sinA.ToString("0.00");
+            lSinBeta.Text = "Sin β = " + sinB.ToString("0.00");
+            lSinGama.Text = "Sin γ = " + sinC.ToString("0.00");
+
+            //tan alpha, beta, gamma;
+            double tanA = Math.Abs(Math.Tan(angA));
+            double tanB = Math.Abs(Math.Tan(angB));
+            double tanC = Math.Abs(Math.Tan(angC));
+            lTanAlpha.Text = "tan α = " + tanA.ToString("0.00");
+            lTanBeta.Text = "tan β = " + tanB.ToString("0.00");
+            lTanGama.Text = "tan γ = " + tanC.ToString("0.00");
+        }
+        int n = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (n == 1) Refresh();
+            if (cbCircle.Checked)
+            {
+                n++;
+                Graphics g = CreateGraphics();
+                int x = ClientRectangle.Width / 2 - (ClientRectangle.Width / 2) / 2 + 90;
+                int y = ClientRectangle.Height / 2 - (ClientRectangle.Width / 2) / 2;
+                CrtanjeKruz(x, y, g);
+            }
+        }
+    }
+}
