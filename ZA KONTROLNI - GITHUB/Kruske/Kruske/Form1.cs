8623346fdc92be8f93b0d68182af7db4612deba8
Aleksa Peštalić <aleksapestalic@gmail.com>
2017-03-29 16:28:00 +0200

    Add files via upload

---
+++ ZA KONTROLNI - GITHUB/Kruske/Kruske/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kruske
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //na mestu klika se pojavljuje kruska random velicine koja pocinje da pada random brzinom.
+        struct Kruska
+        {
+            public int x, y, a, vy;// vy = brzina padanja
+        }
+
+        void Draw(Graphics g, Kruska k)
+        {
+            SolidBrush cetka = new SolidBrush(Color.Orange);
+            //koordinate dve elipse
+            g.FillEllipse(cetka, k.x - k.a, k.y + k.a, 2 * k.a, 2 * k.a);
+            g.FillEllipse(cetka, k.x - k.a / 2, k.y, k.a, 3 * k.a);
+            //promena boje cetke
+            cetka.Color = Color.YellowGreen;
+            g.FillEllipse(cetka, k.x, k.y - k.a, k.a, k.a / 2);
+            //inicijalizacija olovke za crtaanje peteljke
+            Pen olovka = new Pen(Color.Brown, 3);
+            g.DrawLine(olovka, k.x, k.y - k.a, k.x, k.y);
+        }
+
+        //inicijalizacija niza krusaka
+        Kruska[] k = new Kruska[50];
+        int n = 0;
+
+        Random r = new Random();
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //svaki put se menjaju vrednosti dok ne bude 50 krusaka nacrtano?
+            if (n < 50)
+            {
+                k[n].a = r.Next(20, 40);
+                k[n].vy = r.Next(1, 7);
+                k[n].x = e.X;
+                k[n].y = e.Y;
+                n++;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                //dok se ne iscrta n puta ponavljace se metoda
+                Draw(e.Graphics, k[i]);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                k[i].y += k[i].vy;
+                if (k[i].y > ClientRectangle.Height - 3 * k[i].a)
+                    k[i].vy = 0;
+            }
+            Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
---
+++ ZA KONTROLNI - GITHUB/Kuca nadesno/Kuca nadesno/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kuca_nadesno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        //po pokretanju se pojavi 20 kuca koje se pomeraju nadesno...
+        
+        struct Kuca
+        {
+            public int x, y, a, vx;
+            public Color boja;
+        }
+
+        void Draw(Graphics g, Kuca k)
+        {
+            //koordinate i draw
+            SolidBrush cetka = new SolidBrush(k.boja);
+            g.FillRectangle(cetka, k.x - k.a, k.y, 2 * k.a, 2 * k.a);
+            cetka.Color = Color.Red;
+            Point p1 = new Point(k.x - k.a, k.y);
+            Point p2 = new Point(k.x, k.y - k.a);
+            Point p3 = new Point(k.x + k.a, k.y);
+            Point[] krov = { p1, p2, p3 };
+            g.FillPolygon(cetka, krov);
+        }
+        Kuca[] k = new Kuca[20]; // vrednosti se mogu menjati, ali uvek mora k == n!;
+        int n = 20;
+
+        Random r = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                //deklarisu se vrednosti variabli
+                k[i].x = r.Next(ClientRectangle.Width);
+                k[i].y = r.Next(ClientRectangle.Height); // menjaju se za svaki crtez
+                k[i].a = r.Next(10, 20);
+                k[i].vx = 5;
+                k[i].boja = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+            }
+            timer1.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //ciklus koji crta
+            for (int i = 0; i < n; i++)
+            {
+                Draw(e.Graphics, k[i]); // k[i] znaci da se svakim obrtom ciklusa pomeri pokazivac u nizu za 1...
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // svakim obrtom ciklusa var x se poveca za var vx...
+            for (int i = 0; i < n; i++)
+            {
+                k[i].x += k[i].vx;
+            }
+            Refresh();
+        }
+
+    }
+}
